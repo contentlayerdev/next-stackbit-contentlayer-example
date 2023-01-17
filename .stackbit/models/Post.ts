c0e24e233ee56466c968d384ab4258aa54c39114
Sean C Davis <scdavis41@gmail.com>
2023-01-17 15:58:24 -0500

    Add post model

---
+++ .stackbit/models/Post.ts
@@ -0,0 +1,19 @@
+import type * as Stackbit from "@stackbit/sdk"
+
+export const Post: Stackbit.YamlPageModel = {
+  type: "page",
+  urlPath: "/posts/{slug}",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      default: "Post title and main heading",
+      required: true,
+    },
+    {
+      name: "date",
+      type: "date",
+      description: "Publish date",
+    },
+  ],
+}
--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,10 +1,12 @@
 import type * as Stackbit from "@stackbit/sdk"
+import { Post } from "./.stackbit/models/Post"
 
 const stackbitConfig: Stackbit.RawConfig = {
   stackbitVersion: "~0.5.0",
   cmsName: "git",
   ssgName: "nextjs",
   pagesDir: "content/pages",
+  models: { Post },
   customContentReload: true,
 }
 
