4cd5968f58844c4a8b0d7d1e18ccd160b6bc0489
Sean C Davis <scdavis41@gmail.com>
2023-01-19 11:41:51 -0500

    Update contentlayer configuration

---
+++ contentlayer.config.ts
@@ -0,0 +1,22 @@
+import { makeSource } from 'contentlayer/source-files'
+import { stackbitConfigToDocumentTypes } from '@contentlayer/experimental-source-files-stackbit'
+import stackbitConfig from './stackbit.config'
+
+const documentTypes = stackbitConfigToDocumentTypes(stackbitConfig, {
+  documentTypes: {
+    Post: {
+      computedFields: {
+        id: {
+          type: 'string',
+          resolve: (doc) => `content/${doc._raw.sourceFilePath}`,
+        },
+        url: {
+          type: 'string',
+          resolve: (doc) => doc._raw.flattenedPath.replace(/^pages\//, '/'),
+        },
+      },
+    },
+  },
+})
+
+export default makeSource({ contentDirPath: 'content', documentTypes })
