78b4b48052a082048f238b54f8638a254000e331
Sean C Davis <scdavis41@gmail.com>
2023-01-19 10:55:44 -0500

    Remove annotations

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,6 +1,6 @@
-import Head from "next/head"
-import { format, parseISO } from "date-fns"
-import { allPosts, Post } from "contentlayer/generated"
+import Head from 'next/head'
+import { format, parseISO } from 'date-fns'
+import { allPosts, Post } from 'contentlayer/generated'
 
 export async function getStaticPaths() {
   const paths: string[] = allPosts.map((post) => post.url)
@@ -21,25 +21,18 @@ export async function getStaticProps({ params }) {
 
 const PostLayout = ({ post }: { post: Post }) => {
   return (
-    <div data-sb-object-id={post.id}>
+    <div>
       <Head>
         <title>{post.title}</title>
       </Head>
       <article className="max-w-xl py-8 mx-auto">
         <div className="mb-8 text-center">
-          <time
-            dateTime={post.date}
-            className="mb-1 text-xs text-gray-600 dark:text-slate-500"
-            data-sb-field-path="date"
-          >
-            {format(parseISO(post.date), "LLLL d, yyyy")}
+          <time dateTime={post.date} className="mb-1 text-xs text-gray-600 dark:text-slate-500">
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
           </time>
-          <h1 data-sb-field-path="title">{post.title}</h1>
+          <h1>{post.title}</h1>
         </div>
-        <div
-          dangerouslySetInnerHTML={{ __html: post.body.html }}
-          data-sb-field-path="markdown_content"
-        />
+        <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
       </article>
     </div>
   )
