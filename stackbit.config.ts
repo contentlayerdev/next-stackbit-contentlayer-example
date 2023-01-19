cce85402b85cf5562750c25b2b0ccb80c170d833
Sean C Davis <scdavis41@gmail.com>
2023-01-19 11:26:43 -0500

    Add post model

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,5 @@
 import type * as Stackbit from '@stackbit/sdk'
+import { Post } from './.stackbit/models/Post'
 
 const stackbitConfig: Stackbit.RawConfig = {
   stackbitVersion: '~0.5.0',
@@ -6,6 +7,7 @@ const stackbitConfig: Stackbit.RawConfig = {
   ssgName: 'nextjs',
   pagesDir: 'content/pages',
   pageLayoutKey: 'type',
+  models: { Post },
   customContentReload: true,
 }
 
