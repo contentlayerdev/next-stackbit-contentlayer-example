97a6ec90b1c4c55dc2a2ae90537bf86312603581
Sean C Davis <scdavis41@gmail.com>
2023-01-19 10:55:46 -0500

    A bit more styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link"
-import { compareDesc, format, parseISO } from "date-fns"
-import { allPosts, Post } from "contentlayer/generated"
+import Link from 'next/link'
+import { compareDesc, format, parseISO } from 'date-fns'
+import { allPosts, Post } from 'contentlayer/generated'
 
 export async function getStaticProps() {
   const posts: Post[] = allPosts.sort((a, b) => {
@@ -11,14 +11,14 @@ export async function getStaticProps() {
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-8">
+    <div className="py-6 border-b border-b-gray-600 last:border-b-0">
       <h2 className="text-xl">
         <Link href={post.url} className="text-blue-700 hover:text-blue-900" legacyBehavior>
           {post.title}
         </Link>
       </h2>
       <time dateTime={post.date} className="block mb-2 text-xs text-gray-600 dark:text-slate-500">
-        {format(parseISO(post.date), "LLLL d, yyyy")}
+        {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
       <div className="text-sm" dangerouslySetInnerHTML={{ __html: post.body.html }} />
     </div>
